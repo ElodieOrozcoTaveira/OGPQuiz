b443ae3e9224975e5bb05797ed74b2a022f29161
Elodie Orozco Taveira <elodie.orozco-taveira@oclock.school>
2025-05-07 23:16:56 +0200

    header pratiquement terminé, à modifier selon les goutr selon les goutes :)

---
+++ FRONT/src/App.tsx
@@ -0,0 +1,16 @@
+import './sass/styles/index.css'
+import Header from './Components/Header'
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      
+     
+    </div>
+  )
+   
+}
+
+export default App
